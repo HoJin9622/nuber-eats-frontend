4e7ce400ffd3e2b13820f371fbcc59ab74c2af15
이호진 <kiss0104040@gmail.com>
2021-08-09 21:00:25 +0900

    15.1 React Hoom Form

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,15 +1,49 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { isLoggedInVar } from '../apollo'
 
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true)
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+  const onSubmit = () => {
+    console.log(watch('email'))
   }
+  const onInvalid = () => {
+    console.log('cant create account')
+  }
+
+  console.log(errors)
 
   return (
     <div>
       <span>Logged Out</span>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            {...register('email', {
+              required: 'This is required',
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name='email'
+            type='email'
+            placeholder='email'
+          />
+        </div>
+        <div>
+          <input
+            {...register('password', { required: true })}
+            name='password'
+            type='password'
+            required
+            placeholder='password'
+          />
+        </div>
+        <button className='bg-yellow-300 text-white'>Submit</button>
+      </form>
     </div>
   )
 }
