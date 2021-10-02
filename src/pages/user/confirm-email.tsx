487b598dc841ee1f337356ef44791023c461a963
이호진 <kiss0104040@gmail.com>
2021-10-02 22:24:53 +0900

    16.2 Edit Profile part One

--- src/pages/user/confirm-email.tsx
+++ src/pages/user/confirm-email.tsx
@@ -1,6 +1,7 @@
 import { useApolloClient, useMutation } from '@apollo/client'
 import gql from 'graphql-tag'
 import React, { useEffect } from 'react'
+import { useHistory } from 'react-router'
 import { useMe } from '../../hooks/useMe'
 import {
   verifyEmail,
@@ -19,6 +20,7 @@ const VERIFY_EMAIL_MUTATION = gql`
 export const ConfirmEamil = () => {
   const { data: userData } = useMe()
   const client = useApolloClient()
+  const history = useHistory()
   const onCompleted = (data: verifyEmail) => {
     const {
       verifyEmail: { ok },
@@ -33,6 +35,7 @@ export const ConfirmEamil = () => {
         `,
         data: { verified: true },
       })
+      history.push('/')
     }
   }
   const [verifyEmail] = useMutation<verifyEmail, verifyEmailVariables>(
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -6,6 +6,7 @@ import { Header } from '../components/header'
 import { useMe } from '../hooks/useMe'
 import { NotFound } from '../pages/404'
 import { ConfirmEamil } from '../pages/user/confirm-email'
+import { EditProfile } from '../pages/user/edit-profile'
 
 const ClientRoutes = [
   <Route key={1} path='/' exact>
@@ -13,6 +14,9 @@ const ClientRoutes = [
   </Route>,
   <Route key={2} path='/confirm' exact>
     <ConfirmEamil />
+  </Route>,
+  <Route key={3} path='/edit-profile' exact>
+    <EditProfile />
   </Route>,
 ]
 
