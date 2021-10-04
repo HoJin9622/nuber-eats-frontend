a639615c231d224b79d0904b54e5e91c470325ec
이호진 <kiss0104040@gmail.com>
2021-10-04 15:58:27 +0900

    17.4 Search part One

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
-import React from 'react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useHistory } from 'react-router'
+import { Restaurant } from '../../components/restaurant'
 import {
   restaurantsPageQuery,
   restaurantsPageQueryVariables,
@@ -38,30 +41,50 @@ const RESTAURANTS_QUERY = gql`
   }
 `
 
+interface IFormProps {
+  searchTerm: string
+}
+
 export const Restaurants = () => {
+  const [page, setPage] = useState(1)
   const { data, loading } = useQuery<
     restaurantsPageQuery,
     restaurantsPageQueryVariables
   >(RESTAURANTS_QUERY, {
     variables: {
-      input: { page: 1 },
+      input: { page },
     },
   })
-  console.log(data)
+  const onNextPageClick = () => setPage((current) => current + 1)
+  const onPrevPageClick = () => setPage((current) => current - 1)
+  const { register, handleSubmit, getValues } = useForm<IFormProps>()
+  const history = useHistory()
+  const onSearchSubmit = () => {
+    const { searchTerm } = getValues()
+    history.push({ pathname: 'search', search: `?term=${searchTerm}` })
+  }
   return (
     <div>
-      <form className='bg-gray-800 w-full py-40 flex items-center justify-center'>
+      <form
+        onSubmit={handleSubmit(onSearchSubmit)}
+        className='bg-gray-800 w-full py-40 flex items-center justify-center'
+      >
         <input
+          {...register('searchTerm', { required: true, min: 3 })}
+          name='searchTerm'
           type='Search'
-          className='input rounded-md border-0 w-3/12'
+          className='input rounded-md border-0 w-3/4 md:w-3/12'
           placeholder='Search Restaurants...'
         />
       </form>
       {!loading && (
-        <div className='max-w-screen-2xl mx-auto mt-8'>
+        <div className='max-w-screen-2xl pb-20 mx-auto mt-8'>
           <div className='flex justify-around max-w-sm mx-auto'>
             {data?.allCategories.categories?.map((category) => (
-              <div className='flex flex-col group items-center cursor-pointer'>
+              <div
+                key={category.id}
+                className='flex flex-col group items-center cursor-pointer'
+              >
                 <div
                   className='w-16 h-16 bg-cover group-hover:bg-gray-100 rounded-full'
                   style={{
@@ -74,19 +97,41 @@ export const Restaurants = () => {
               </div>
             ))}
           </div>
-          <div className='grid mt-10 grid-cols-3 gap-x-5 gap-y-10'>
+          <div className='grid mt-10 md:grid-cols-3 gap-x-5 gap-y-10'>
             {data?.restaurants.results?.map((restaurant) => (
-              <div>
-                <div
-                  style={{ backgroundImage: `url(${restaurant.coverImg})` }}
-                  className='bg-red-500 bg-cover bg-center mb-3 py-28'
-                ></div>
-                <h3 className='text-xl font-medium'>{restaurant.name}</h3>
-                <span className='border-t-2 border-gray-200'>
-                  {restaurant.category?.name}
-                </span>
-              </div>
+              <Restaurant
+                key={restaurant.id}
+                id={restaurant.id + ''}
+                name={restaurant.name}
+                coverImg={restaurant.coverImg}
+                categoryName={restaurant.category?.name}
+              />
             ))}
+          </div>
+          <div className='grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10'>
+            {page > 1 ? (
+              <button
+                onClick={onPrevPageClick}
+                className='focus:outline-none font-medium text-2xl'
+              >
+                &larr;
+              </button>
+            ) : (
+              <div></div>
+            )}
+            <span>
+              Page {page} of {data?.restaurants.totalPages}
+            </span>
+            {page !== data?.restaurants.totalPages ? (
+              <button
+                onClick={onNextPageClick}
+                className='focus:outline-none font-medium text-2xl'
+              >
+                &rarr;
+              </button>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       )}
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -7,6 +7,7 @@ import { useMe } from '../hooks/useMe'
 import { NotFound } from '../pages/404'
 import { ConfirmEamil } from '../pages/user/confirm-email'
 import { EditProfile } from '../pages/user/edit-profile'
+import { Search } from '../pages/client/search'
 
 const ClientRoutes = [
   <Route key={1} path='/' exact>
@@ -17,6 +18,9 @@ const ClientRoutes = [
   </Route>,
   <Route key={3} path='/edit-profile' exact>
     <EditProfile />
+  </Route>,
+  <Route key={3} path='/search'>
+    <Search />
   </Route>,
 ]
 
