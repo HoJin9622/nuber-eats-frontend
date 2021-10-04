9de1fdbf71241a67ee5d5f4bc4b10eea61bc20bc
이호진 <kiss0104040@gmail.com>
2021-10-04 18:13:20 +0900

    17.5 Search part Two

--- src/pages/client/restaurants.tsx
+++ src/pages/client/restaurants.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@apollo/client'
 import gql from 'graphql-tag'
 import React, { useState } from 'react'
+import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { useHistory } from 'react-router'
 import { Restaurant } from '../../components/restaurant'
+import { RESTAURANT_FRAGMENT } from '../../fragments'
 import {
   restaurantsPageQuery,
   restaurantsPageQueryVariables,
@@ -28,17 +30,11 @@ const RESTAURANTS_QUERY = gql`
       totalPages
       totalResults
       results {
-        id
-        name
-        coverImg
-        category {
-          name
-        }
-        address
-        isPromoted
+        ...RestaurantParts
       }
     }
   }
+  ${RESTAURANT_FRAGMENT}
 `
 
 interface IFormProps {
@@ -65,6 +61,9 @@ export const Restaurants = () => {
   }
   return (
     <div>
+      <Helmet>
+        <title>Home | Nuber Eats</title>
+      </Helmet>
       <form
         onSubmit={handleSubmit(onSearchSubmit)}
         className='bg-gray-800 w-full py-40 flex items-center justify-center'
