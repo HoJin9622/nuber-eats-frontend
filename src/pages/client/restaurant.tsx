fd26ddf0a8e7c976e16da9c0b960ebac107ac8cb
이호진 <kiss0104040@gmail.com>
2021-10-04 21:57:04 +0900

    17.9 Restaurant part Two

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -28,5 +28,24 @@ export const Restaurant = () => {
     RESTAURANT_QUERY,
     { variables: { input: { restaurantId: +params.id } } }
   )
-  return <h1>Restaurant</h1>
+  return (
+    <div>
+      <div
+        className='bg-gray-800 bg-center bg-cover py-48'
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className='bg-white w-3/12 py-8 pl-48'>
+          <h4 className='text-3xl mb-3'>{data?.restaurant.restaurant?.name}</h4>
+          <h5 className='text-sm font-light mb-2'>
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className='text-sm font-light'>
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+  )
 }
