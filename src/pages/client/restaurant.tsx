64e08dc655c34851f02cd9c51dd0e139c16411b5
이호진 <kiss0104040@gmail.com>
2021-12-23 00:37:03 +0900

    #22.3 Making Order part Three

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -48,17 +48,33 @@ export const Restaurant = () => {
   const triggerStartOrder = () => {
     setOrderStarted(true)
   }
+  const getItem = (dishId: number) => {
+    return orderItems.find((order) => order.dishId === dishId)
+  }
   const isSelected = (dishId: number) => {
-    return Boolean(orderItems.find((order) => order.dishId === dishId))
+    return Boolean(getItem(dishId))
   }
   const addItemToOrder = (dishId: number) => {
     if (isSelected(dishId)) {
       return
     }
-    setOrderItems((current) => [{ dishId }, ...current])
+    setOrderItems((current) => [{ dishId, options: [] }, ...current])
   }
   const removeFromOrder = (dishId: number) => {
     setOrderItems((current) => current.filter((dish) => dish.dishId !== dishId))
+  }
+  const addOptionToItem = (dishId: number, options: any) => {
+    if (!isSelected(dishId)) {
+      return
+    }
+    const oldItem = getItem(dishId)
+    if (oldItem) {
+      removeFromOrder(dishId)
+      setOrderItems((current) => [
+        { dishId, options: [options, ...oldItem.options!] },
+        ...current,
+      ])
+    }
   }
   console.log(orderItems)
 
@@ -98,6 +114,7 @@ export const Restaurant = () => {
               options={dish.options}
               addItemToOrder={addItemToOrder}
               removeFromOrder={removeFromOrder}
+              addOptionToItem={addOptionToItem}
             />
           ))}
         </div>
