5ee5e0011989dd8887ce2a1f89120d2e605630fd
이호진 <kiss0104040@gmail.com>
2021-12-23 22:50:14 +0900

    #22.4 Making Order part Four

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -63,17 +63,34 @@ export const Restaurant = () => {
   const removeFromOrder = (dishId: number) => {
     setOrderItems((current) => current.filter((dish) => dish.dishId !== dishId))
   }
-  const addOptionToItem = (dishId: number, options: any) => {
+  const addOptionToItem = (dishId: number, option: any) => {
     if (!isSelected(dishId)) {
       return
     }
     const oldItem = getItem(dishId)
     if (oldItem) {
-      removeFromOrder(dishId)
-      setOrderItems((current) => [
-        { dishId, options: [options, ...oldItem.options!] },
-        ...current,
-      ])
+      const hasOption = Boolean(
+        oldItem.options?.find((aOption) => aOption.name == option.name)
+      )
+      if (!hasOption) {
+        removeFromOrder(dishId)
+        setOrderItems((current) => [
+          { dishId, options: [option, ...oldItem.options!] },
+          ...current,
+        ])
+      }
+    }
+  }
+  const getOptionFromItem = (
+    item: CreateOrderItemInput,
+    optionName: string
+  ) => {
+    return item.options?.find((option) => option.name === optionName)
+  }
+  const isOptionSelected = (dishId: number, optionName: string) => {
+    const item = getItem(dishId)
+    if (item) {
+      return Boolean(getOptionFromItem(item, optionName))
     }
   }
   console.log(orderItems)
@@ -114,8 +131,26 @@ export const Restaurant = () => {
               options={dish.options}
               addItemToOrder={addItemToOrder}
               removeFromOrder={removeFromOrder}
-              addOptionToItem={addOptionToItem}
-            />
+            >
+              {dish.options?.map((option, index) => (
+                <span
+                  onClick={() =>
+                    addOptionToItem
+                      ? addOptionToItem(dish.id, { name: option.name })
+                      : null
+                  }
+                  className={`flex border items-center ${
+                    isOptionSelected(dish.id, option.name)
+                      ? 'border-gray-800'
+                      : ''
+                  }`}
+                  key={index}
+                >
+                  <h6 className='mr-2'>{option.name}</h6>
+                  <h6 className='text-sm opacity-75'>(${option.extra})</h6>
+                </span>
+              ))}
+            </Dish>
           ))}
         </div>
       </div>
