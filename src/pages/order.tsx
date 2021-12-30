ff9b3ab79b740ccc7f25f41b1982427324ffc9d0
이호진 <kiss0104040@gmail.com>
2021-12-30 19:01:06 +0900

    #23.1 Subscription Setup

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,8 +1,14 @@
-import { gql } from '@apollo/client'
+import { gql, useSubscription } from '@apollo/client'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@apollo/client'
 import { getOrder, getOrderVariables } from '../__generated__/getOrder'
+import { Helmet } from 'react-helmet-async'
+import { FULL_ORDER_FRAGMENT } from '../fragments'
+import {
+  orderUpdates,
+  orderUpdatesVariables,
+} from '../__generated__/orderUpdates'
 
 const GET_ORDER = gql`
   query getOrder($input: GetOrderInput!) {
@@ -10,21 +16,21 @@ const GET_ORDER = gql`
       ok
       error
       order {
-        id
-        status
-        total
-        driver {
-          email
-        }
-        customer {
-          email
-        }
-        restaurant {
-          name
-        }
+        ...FullOrderParts
       }
     }
   }
+  ${FULL_ORDER_FRAGMENT}
+`
+
+const ORDER_SUBSCRIPTION = gql`
+  subscription orderUpdates($input: OrderUpdatesInput!) {
+    orderUpdates(input: $input) {
+      ...FullOrderParts
+    }
+  }
+
+  ${FULL_ORDER_FRAGMENT}
 `
 
 interface IParams {
@@ -36,6 +42,48 @@ export const Order = () => {
   const { data } = useQuery<getOrder, getOrderVariables>(GET_ORDER, {
     variables: { input: { id: +params.id } },
   })
-  console.log(data)
-  return <h1>{params.id}</h1>
+  const { data: subscriptionData } = useSubscription<
+    orderUpdates,
+    orderUpdatesVariables
+  >(ORDER_SUBSCRIPTION, { variables: { input: { id: +params.id } } })
+
+  console.log(subscriptionData)
+  return (
+    <div className='mt-32 container flex justify-center'>
+      <Helmet>
+        <title>Order #{params.id} | Nuber Eats</title>
+      </Helmet>
+      <div className='border border-gray-800 w-full max-w-screen-sm flex flex-col justify-center'>
+        <h4 className='bg-gray-800 w-full py-5 text-white text-center text-xl'>
+          Order #{params.id}
+        </h4>
+        <h5 className='p5 pt-10 text-3xl text-center'>
+          ${data?.getOrder.order?.total}
+        </h5>
+        <div className='p-5 text-xl grid gap-6'>
+          <div className=' border-t pt-5 border-gray-700'>
+            Prepared By:{' '}
+            <span className='font-medium'>
+              {data?.getOrder.order?.restaurant?.name}
+            </span>
+          </div>
+          <div className='border-t pt-5 border-gray-700'>
+            Deliver To:{' '}
+            <span className='font-medium'>
+              {data?.getOrder.order?.customer?.email}
+            </span>
+          </div>
+          <div className='border-t border-b py-5 border-gray-700'>
+            Driver:{' '}
+            <span className='font-medium'>
+              {data?.getOrder.order?.driver?.email || 'Not yet.'}
+            </span>
+          </div>
+          <span className=' text-center mt-5 mb-3 text-2xl text-lime-600'>
+            Status: {data?.getOrder.order?.status}
+          </span>
+        </div>
+      </div>
+    </div>
+  )
 }
