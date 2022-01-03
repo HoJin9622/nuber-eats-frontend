da4c698d0a22ee4201c63f56dae91d87f2ef62e6
이호진 <kiss0104040@gmail.com>
2022-01-03 23:47:53 +0900

    #23.7 Drier Dashboard part Three

--- src/pages/driver/dashboard.tsx
+++ src/pages/driver/dashboard.tsx
@@ -11,6 +11,8 @@ export const Dashboard = () => {
     lng: 129.0814828,
     lat: 35.1651525,
   })
+  const [map, setMap] = useState<any>()
+  const [maps, setMaps] = useState<any>()
   const onSuccess = ({
     coords: { latitude, longitude },
   }: GeolocationPosition) => {
@@ -24,22 +26,38 @@ export const Dashboard = () => {
       enableHighAccuracy: true,
     })
   }, [])
+  useEffect(() => {
+    if (map && maps) {
+      map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng))
+    }
+  }, [driverCoords.lat, driverCoords.lng])
   const onApiLoaded = ({ map, maps }: { map: any; maps: any }) => {
     map.panTo(new maps.LatLng(driverCoords.lat, driverCoords.lng))
+    setMap(map)
+    setMaps(maps)
   }
   return (
     <div>
       <div
         className='overflow-hidden'
-        style={{ width: window.innerWidth, height: '95vh' }}
+        style={{ width: window.innerWidth, height: '50vh' }}
       >
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBpv3mxWUebIfBoH2nt0qzIni0dWl8FkVw' }}
-          defaultZoom={15}
+          defaultZoom={16}
           defaultCenter={{ lat: 36, lng: 125 }}
           yesIWantToUseGoogleMapApiInternals
           onGoogleApiLoaded={onApiLoaded}
-        ></GoogleMapReact>
+        >
+          <div
+            // @ts-ignore
+            lat={driverCoords.lat}
+            lng={driverCoords.lng}
+            className='text-lg'
+          >
+            🚖
+          </div>
+        </GoogleMapReact>
       </div>
     </div>
   )
